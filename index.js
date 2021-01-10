09f28add8ae1c5430f4c7683c49703337a0e6fa5
heissonwillen <heisson70@gmail.com>
2021-01-10 12:52:43 -0300

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("dotenv/config");
-require("dotenv/lib/env-options");
+
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
-const flipCoin = () => {
-  return Math.floor(Math.random() * 2);
-};
+const captions = ["Cara", "Coroa"];
 
 bot.onText(/\/flip/, (msg, match) => {
   const chatId = msg.chat.id;
-  console.log("Received");
-  const result = flipCoin();
-  bot.sendPhoto(chatId, `img/${result}.png`, { caption: ["Cara", "Coroa"][result] });
+  const result = Math.floor(Math.random() * 2);
+  const photoPath = `img/${result}.png`;
+
+  bot.sendPhoto(chatId, photoPath, {
+    caption: captions[result],
+  });
 });
