ef77707350571655f2845a4342f4eae2732dbd6d
heissonwillen <heisson70@gmail.com>
2021-01-10 12:11:34 -0300

    fix typo (index.js)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv/config");
-const options = require("dotenv/lib/env-options");
+require("dotenv/lib/env-options");
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
