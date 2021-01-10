fb9dc1bf912beec2fdadffade36b8ee957c98338
heissonwillen <heisson70@gmail.com>
2021-01-10 12:10:36 -0300

    add first implementation

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv/config");
+const options = require("dotenv/lib/env-options");
+const TelegramBot = require("node-telegram-bot-api");
+const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
+const flipCoin = () => {
+  return Math.floor(Math.random() * 2);
+};
+
+bot.onText(/\/flip/, (msg, match) => {
+  const chatId = msg.chat.id;
+  console.log("Received");
+  const result = flipCoin();
+  bot.sendPhoto(chatId, `img/${result}.png`, { caption: ["Cara", "Coroa"][result] });
+});
