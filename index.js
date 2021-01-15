5d9cd20531c70c0cbd08bbf3a6a2b590d01320fd
heissonwillen <heisson70@gmail.com>
2021-01-15 13:20:47 -0300

    add dice

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ require("dotenv/config");
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
-const captions = ["Cara", "Coroa"];
-
-bot.onText(/\/flip/, (msg, match) => {
+bot.onText(/\/flip/, (msg) => {
   const chatId = msg.chat.id;
   const result = Math.floor(Math.random() * 2);
-  const photoPath = `img/${result}.png`;
+  const photoPath = `img/coin/${result}.png`;
 
   bot.sendPhoto(chatId, photoPath, {
-    caption: captions[result],
+    caption:  ["Cara", "Coroa"][result],
+  });
+});
+
+bot.onText(/\/dice/, (msg) => {
+  const chatId = msg.chat.id;
+  const result = Math.floor(Math.random() * 6) + 1;
+  const photoPath = `img/dice/${result}.png`;
+
+  bot.sendPhoto(chatId, photoPath, {
+    caption: String(result),
   });
 });
